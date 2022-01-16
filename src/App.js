479508dfc5fd1e265daa3f4e5b9b89fbb729bbdd
unknown <ghaliarose89@gmail.com>
2022-01-16 17:34:47 -0600

    d

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import Footer from './components/Footer';
-import Projects from './components/Projects'
+
 function App() {
   return (
     <div>
